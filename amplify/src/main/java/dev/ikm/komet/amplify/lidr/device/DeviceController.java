84aef7598af435004ae3b5f71f6187159ff1fd75
Douglas Holubek <dholubek@tinkarbuild.com>
2024-04-23 19:17:51 +0000

    JIRA: [IIA-516] drag and drop for device

--- amplify/src/main/java/dev/ikm/komet/amplify/lidr/device/DeviceController.java
+++ amplify/src/main/java/dev/ikm/komet/amplify/lidr/device/DeviceController.java
@@ -20,16 +20,29 @@ import dev.ikm.komet.amplify.lidr.events.AddDeviceEvent;
 import dev.ikm.komet.amplify.lidr.events.LidrPropertyPanelEvent;
 import dev.ikm.komet.amplify.mvvm.loader.InjectViewModel;
 import dev.ikm.komet.amplify.lidr.viewmodels.DeviceViewModel;
+import dev.ikm.komet.framework.Identicon;
 import dev.ikm.komet.framework.events.EvtBus;
 import dev.ikm.komet.framework.events.EvtBusFactory;
 import dev.ikm.komet.framework.view.ViewProperties;
+import dev.ikm.komet.navigator.graph.MultiParentGraphCell;
+import dev.ikm.tinkar.terms.ConceptFacade;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,6 +50,7 @@ import java.util.UUID;
 
 import static dev.ikm.komet.amplify.lidr.events.AddDeviceEvent.ADD_DEVICE;
 import static dev.ikm.komet.amplify.lidr.events.LidrPropertyPanelEvent.CLOSE_PANEL;
+import static dev.ikm.komet.amplify.lidr.viewmodels.DeviceViewModel.DEVICE_ENTITY;
 import static dev.ikm.komet.amplify.viewmodels.FormViewModel.CONCEPT_TOPIC;
 import static dev.ikm.komet.amplify.viewmodels.FormViewModel.VIEW_PROPERTIES;
 
@@ -55,6 +69,13 @@ public class DeviceController implements BasicController {
     @FXML
     private Button doneButton;
 
+    @FXML
+    private VBox selectedDeviceContainer;
+
+    @FXML
+    private StackPane selectedDeviceStackPane;
+
+
     @InjectViewModel
     private DeviceViewModel deviceViewModel;
     EvtBus evtBus = EvtBusFactory.getDefaultEvtBus();
@@ -72,12 +93,15 @@ public class DeviceController implements BasicController {
         // setup drag n drop
         setupDragNDrop(deviceDragAndDropArea);
     }
+
     public ViewProperties getViewProperties() {
         return deviceViewModel.getPropertyValue(VIEW_PROPERTIES);
     }
+
     private UUID getConceptTopic() {
         return deviceViewModel.getPropertyValue(CONCEPT_TOPIC);
     }
+
     private void setupDragNDrop(Node node) {
 
         // when gesture is dragged over node
@@ -120,6 +144,12 @@ public class DeviceController implements BasicController {
             boolean success = false;
             if (db.hasString()) {
                 String publicId = db.getString(); // TODO find entity by public id.
+
+                // add the component to the device view model
+                ConceptFacade conceptFacade = ((MultiParentGraphCell) event.getGestureSource()).getItem();
+                if (conceptFacade != null && deviceViewModel.getPropertyValue(DEVICE_ENTITY) == null) {
+                    addDeviceToForm(conceptFacade);
+                }
                 success = true;
             }
             /* let the source know whether the string was successfully
@@ -128,6 +158,65 @@ public class DeviceController implements BasicController {
 
             event.consume();
         });
+    }
+
+    /**
+     * create a JavaFX node for the device concept and add it to
+     * above the search form
+     *
+     * @param conceptFacade
+     */
+    private void addDeviceToForm(ConceptFacade conceptFacade) {
+        // add the device to the view model
+        deviceViewModel.setPropertyValue(DEVICE_ENTITY, conceptFacade);
+
+        // update the UI to show the device that the user just dragged (or searched or manually entered)
+        HBox selectedConcept = new HBox();
+
+        // create identicon for the concept and add it to the left hbox
+        Image identicon = Identicon.generateIdenticonImage(conceptFacade.publicId());
+        ImageView imageView = new ImageView();
+        imageView.setFitWidth(20);
+        imageView.setFitHeight(20);
+        imageView.setImage(identicon);
+        HBox imageViewWrapper = new HBox();
+        imageViewWrapper.setAlignment(Pos.CENTER);
+        HBox.setMargin(imageView, new Insets(0, 8, 0 ,8));
+        imageViewWrapper.getChildren().add(imageView);
+        selectedConcept.getChildren().add(imageViewWrapper);
+
+        // create the label
+        String conceptName = conceptFacade.description();
+        Label conceptNameLabel = new Label(conceptName);
+        conceptNameLabel.getStyleClass().add("lidr-device-entry-label");
+        selectedConcept.getChildren().add(conceptNameLabel);
+
+        // format the device HBox
+        selectedConcept.getStyleClass().add("lidr-device-entry");
+        selectedConcept.setAlignment(Pos.CENTER_LEFT);
+        selectedConcept.setPadding(new Insets(4, 0, 4, 0));
+        HBox.setMargin(selectedDeviceContainer, new Insets(8));
+
+        // add the close 'X' button to the right side of the device container
+        Button closeButton = new Button();
+        closeButton.getStyleClass().add("lidr-search-button");
+        Region buttonRegion = new Region();
+        buttonRegion.getStyleClass().add("lidr-device-entry-close-button");
+        closeButton.setGraphic(buttonRegion);
+        closeButton.setAlignment(Pos.CENTER_RIGHT);
+        selectedConcept.getChildren().add(closeButton);
+        closeButton.setOnMouseClicked(event -> removeDevice(selectedConcept));
+
+        selectedDeviceContainer.getChildren().add(selectedConcept);
+
+        VBox.setMargin(selectedDeviceStackPane, new Insets(0,0, 8,0));
+    }
+
+    private void removeDevice(HBox selectedConcept) {
+        deviceViewModel.setPropertyValue(DEVICE_ENTITY, null);
+        selectedDeviceContainer.getChildren().remove(selectedConcept);
+        HBox.setMargin(selectedDeviceContainer, new Insets(0));
+        VBox.setMargin(selectedDeviceStackPane, new Insets(0));
     }
 
     @FXML
