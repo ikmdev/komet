6ff65b30f427c81412a04e4f736522a375be6aef
Doug Holubek <dholubek@deloitte.com>
2024-08-16 14:22:23 -0400

    changing to a record

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/model/PatternField.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/model/PatternField.java
@@ -17,74 +17,16 @@ package dev.ikm.komet.kview.mvvm.model;
 
 import dev.ikm.tinkar.terms.EntityFacade;
 
-public class PatternField {
+/**
+ * object to capture data for a Pattern>Field
+ * @param fieldOrder order in 1..n
+ * @param displayName display name
+ * @param dataType tinkar data type
+ * @param purpose purpose concept
+ * @param meaning meaning concept
+ * @param comments user comments
+ */
+public record PatternField(int fieldOrder, String displayName, EntityFacade dataType,
+                          EntityFacade purpose, EntityFacade meaning, String comments) {
 
-    private int fieldOrder;
-    private String displayName;
-
-    private EntityFacade dataType;
-
-    private EntityFacade purpose;
-
-    private EntityFacade meaning;
-
-    private String comments;
-
-    public PatternField(int fieldOrder, String displayName, EntityFacade dataType,
-                        EntityFacade purpose, EntityFacade meaning, String comments) {
-        this.fieldOrder = fieldOrder;
-        this.displayName = displayName;
-        this.dataType = dataType;
-        this.purpose = purpose;
-        this.meaning = meaning;
-        this.comments = comments;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public int getFieldOrder() {
-        return fieldOrder;
-    }
-
-    public void setFieldOrder(int fieldOrder) {
-        this.fieldOrder = fieldOrder;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public EntityFacade getDataType() {
-        return dataType;
-    }
-
-    public void setDataType(EntityFacade dataType) {
-        this.dataType = dataType;
-    }
-
-    public EntityFacade getPurpose() {
-        return purpose;
-    }
-
-    public void setPurpose(EntityFacade purpose) {
-        this.purpose = purpose;
-    }
-
-    public EntityFacade getMeaning() {
-        return meaning;
-    }
-
-    public void setMeaning(EntityFacade meaning) {
-        this.meaning = meaning;
-    }
 }
