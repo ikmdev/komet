15adaf32f36e6b4549dcf9654eb784053d28f58a
Doug Holubek <dholubek@users.noreply.github.com>
2024-09-10 09:32:34 -0500

    bugfix/IIA-921 export date dropdown match figma (#40)
    * initial commit... styling on date drop down broken and needs work
    
    * work in progress
    
    * latest wip
    
    * improvements from Carl
    
    * adding cell factory
    
    * remove toString bc it is null
    
    * working on vertical-right aligning green checkmark
    
    * keeping cells same with as combobox base
    
    * Created a helper method to support combobox having a check mark when selected.
    
    * fixing the color on the file name on the export
    
    * removing dead code
    
    * optimizing per cell
    
    * removing outdated FIXME
    
    * removing commented/dead code
    
    ---------
    
    Co-authored-by: Doug Holubek <dougholubek@gmail.com>
    Co-authored-by: Carl Dea <carl.dea@gmail.com>

--- kview/src/main/java/dev/ikm/komet/kview/fxutils/ComboBoxHelper.java
+++ kview/src/main/java/dev/ikm/komet/kview/fxutils/ComboBoxHelper.java
@@ -15,19 +15,40 @@
  */
 package dev.ikm.komet.kview.fxutils;
 
+import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
+import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
 import javafx.util.Callback;
 import javafx.util.StringConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Function;
 
+/**
+ * A class containing utility methods for common ComboBox creation / setup
+ */
 public class ComboBoxHelper {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ComboBoxHelper.class);
+    private static final String DEFAULT_CHECK_MARK_ICON_REGION = "check-mark";
+
+    /**
+     * set up a comboBox for ConceptEntities
+     * @param comboBox combo box for concepEntities
+     * @param listener invalidation listener
+     * @param displayText a function that applies how to render the display text
+     * @param <T> type ConceptEntity
+     */
     public static <T> void setupComboBox(ComboBox comboBox, InvalidationListener listener, Function<T, String> displayText) {
         comboBox.setConverter(new StringConverter<T>() {
-
             @Override
             public String toString(T conceptEntity) {
                 return displayText.apply(conceptEntity);
@@ -57,7 +78,6 @@ public class ComboBoxHelper {
                         } else {
                             setText(null);
                         }
-
                     }
                 };
             }
@@ -65,5 +85,81 @@ public class ComboBoxHelper {
 
         // register invalidation listener
         comboBox.getSelectionModel().selectedItemProperty().addListener(listener);
+    }
+
+    /**
+     * style a comboBox with a custom graphic icon
+     * @param comboBox comboBox we are setting up
+     */
+    public static <T> void setupComboBoxWithCheckMarkIcon(ComboBox<T> comboBox, Function<T, String> displayText) {
+        setupComboBoxWithIcon(comboBox, displayText, DEFAULT_CHECK_MARK_ICON_REGION);
+    }
+
+    /**
+     * style a comboBox with a custom graphic icon
+     * @param comboBox comboBox we are setting up
+     * @param iconStyleClass A style class representing a Region of an svg for '-fx-shape' attribute.
+     */
+    public static <T> void setupComboBoxWithIcon(ComboBox<T> comboBox, Function<T, String> displayText, String iconStyleClass) {
+        comboBox.setConverter(new StringConverter<T>() {
+
+            @Override
+            public String toString(T domainObject) {
+                return displayText.apply(domainObject);
+            }
+
+            @Override
+            public T fromString(String string) {
+                return null;
+            }
+        });
+
+        // workaround to hide scroll bar. Odd thing happens on the initial popup a vertical scrollbar appears.
+        // TODO: might be another way to fix by using CSS to fix this issue or examine height of popup (ListView).
+        Platform.runLater(() -> {
+            final Node scrollBar = comboBox.lookup(".scroll-bar:vertical");
+            scrollBar.setStyle("-fx-pref-width: 0; -fx-padding: 0;");
+            scrollBar.applyCss();
+            scrollBar.setVisible(false);
+            scrollBar.setOpacity(0);
+            scrollBar.setScaleX(0);
+        });
+
+        // Code to create a HBox with label and region using an SVG shape.
+        comboBox.setCellFactory(lv -> {
+            final ListCell<T> cell = new ListCell<>() {
+                @Override
+                public void updateItem(T item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(item != null ? displayText.apply(item) : null);
+
+                    // Create an HBox (Label, Region)
+                    // set List Cell's graphic as the HBox
+                    if (!empty) {
+                        if (this.isSelected()) {
+                            this.getStyleClass().add("check-mark-selected");
+
+                            // Label of text horizontal grow
+                            Label contentLabel = new Label(displayText.apply(item));
+                            contentLabel.setTextFill(Color.WHITE);
+                            HBox.setHgrow(contentLabel, Priority.ALWAYS);
+                            contentLabel.setMaxWidth(Double.MAX_VALUE);
+
+                            // Create a check mark graphic
+                            Region iconGraphic = new Region();
+                            iconGraphic.getStyleClass().add(iconStyleClass);
+                            HBox customListCell = new HBox(contentLabel, iconGraphic);
+                            setGraphic(customListCell);
+                        } else {
+                            this.getStyleClass().remove("check-mark-selected");
+                            setGraphic(null);
+                        }
+                    } else {
+                        setGraphic(null);
+                    }
+                }
+            };
+            return cell;
+        });
     }
 }
