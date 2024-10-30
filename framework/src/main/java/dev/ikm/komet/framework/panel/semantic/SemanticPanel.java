f5924e8c25fde7f1eef1cbc2110cd56a137cbdfa
Doug Holubek <dougholubek@gmail.com>
2024-10-30 09:19:44 -0400

    removing unnecessary change

--- framework/src/main/java/dev/ikm/komet/framework/panel/semantic/SemanticPanel.java
+++ framework/src/main/java/dev/ikm/komet/framework/panel/semantic/SemanticPanel.java
@@ -58,7 +58,6 @@ public class SemanticPanel extends ComponentIsFinalPanel<
             HBox referencedComponentInfo = new HBox(3);
             referencedComponentInfo.setAlignment(Pos.CENTER_LEFT);
 
-
             ContextMenu contextMenu = new ContextMenu();
             collapsiblePane.setContextMenu(contextMenu);
             MenuItem versionChronologyMenuItem = new MenuItem("Version chronology for: " +
