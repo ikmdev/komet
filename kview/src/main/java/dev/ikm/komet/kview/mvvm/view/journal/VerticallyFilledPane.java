3662577a87074441838755a10d545b90d34a9117
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2024-10-30 14:56:36 +0000

    Fixes IIA-1025: Commented class VerticallyFilledPane

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/journal/VerticallyFilledPane.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/journal/VerticallyFilledPane.java
@@ -4,6 +4,12 @@ import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 
+/**
+ * VerticallyFilledPane works like Pane, it doesn't do any positioning of its children, only resizing.
+ * The difference from Pane is that when resizing its children it sets their height so that they occupy the whole height of the
+ * VerticallyFilledPane. The width of each child is set to their preferred width.
+ * Use case: this "Layout container" was created to be used in Tray Panes so that he content fills the Tray Pane.
+ */
 public class VerticallyFilledPane extends Pane {
 
     @Override
