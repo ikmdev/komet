b26f4bc255bdbe63163e689fb54da13d2d77d8c2
Keith E Campbell <3868866+kec@users.noreply.github.com>
2025-01-05 19:56:46 -0800

    Comments to address the EditableKlComponentField interface, and TODO: regarding inconsistent data types in starter set.

--- knowledge-layout/src/main/java/dev/ikm/komet/layout/component/version/field/KlComponentField.java
+++ knowledge-layout/src/main/java/dev/ikm/komet/layout/component/version/field/KlComponentField.java
@@ -4,6 +4,8 @@ import dev.ikm.tinkar.common.bind.annotations.axioms.ParentConcept;
 import dev.ikm.tinkar.common.bind.annotations.names.RegularName;
 import dev.ikm.tinkar.entity.Entity;
 import dev.ikm.tinkar.entity.EntityVersion;
+import dev.ikm.tinkar.terms.ConceptFacade;
+import dev.ikm.tinkar.terms.TinkarTerm;
 
 /**
  * Represents a field whose value is some type of entity component.
@@ -17,5 +19,15 @@ import dev.ikm.tinkar.entity.EntityVersion;
 @RegularName("Component Field")
 @ParentConcept(KlField.class)
 public non-sealed interface KlComponentField<E extends Entity<V>, V extends EntityVersion> extends KlField<E> {
-
+    /**
+     * TODO: We are inconsistent in our use of <data type>, vs <data type> field...
+     * We need to clean up the starter set to manage this difference. We probably should have a set
+     * of just the data types, without saying it is a field, because the data types can be used in
+     * a variety of contexts, including serialization of non-class content.
+     * @return
+     */
+    @Override
+    default ConceptFacade fieldDataType() {
+        return TinkarTerm.COMPONENT_FIELD;
+    }
 }
--- kview/src/main/java/dev/ikm/komet/kview/klfields/editable/EditableKlComponentField.java
+++ kview/src/main/java/dev/ikm/komet/kview/klfields/editable/EditableKlComponentField.java
@@ -2,5 +2,24 @@ package dev.ikm.komet.kview.klfields.editable;
 
 import dev.ikm.komet.layout.component.version.field.KlComponentField;
 
+/**
+ * TODO: If you want to differentiate which implementations are editable,
+ * I think you should consider a stand alone generic "EditableField" interface, rather than
+ * creating a subtype of every field, one that is read-only, and one that is editable.
+ *
+ * In coding logic, this may require to clauses, but the approach has looser coupling:<br><br>
+ * <pre>{@code
+if (fieldWidget instanceof KlComponentField && fieldWidget instanceof KlEditable) {
+// Do something interesting.
+}
+ * }</pre>
+ * <br><br>Keep Hierarchies Shallow:
+ * Aim for a shallow inheritance hierarchy to reduce complexity.
+ *
+ * <br><br>Consider Design Patterns:
+ * Design patterns such as the Strategy pattern or the Decorator pattern can help you
+ * achieve flexibility and avoid the need for deep inheritance hierarchies.
+ *
+ */
 public interface EditableKlComponentField extends KlComponentField {
 }
