d6f3256a1ec11c37bb5c9fe743931172ec2a23a8
Keith E Campbell <3868866+kec@users.noreply.github.com>
2025-01-07 12:29:27 -0800

    Feature/finished/ikm kec kl4 (#202)
    * Update version to start feature: IKM-KEC-KL4
    
    * Adding in binding examples, and handling of list and set fields better.
    
    * Update version to finish feature: IKM-KEC-KL4

--- knowledge-layout/src/main/java/dev/ikm/komet/layout/component/version/KlConceptVersionPane.java
+++ knowledge-layout/src/main/java/dev/ikm/komet/layout/component/version/KlConceptVersionPane.java
@@ -2,16 +2,16 @@ package dev.ikm.komet.layout.component.version;
 
 import dev.ikm.komet.framework.observable.ObservableConceptVersion;
 /**
- * The {@code KlConceptVersionPaneSingle} interface represents a pane that displays a single version
+ * The {@code KlConceptVersionPane} interface represents a pane that displays a single version
  * of a concept.
  *
  * <p>
- * This interface is a specialization of {@link KlVersionPaneSingle} for handling
+ * This interface is a specialization of {@link KlVersionPane} for handling
  * {@link ObservableConceptVersion} types.
  * </p>
  *
  *
- * @see KlVersionPaneSingle
+ * @see KlVersionPane
  * @see ObservableConceptVersion
  */
 public interface KlConceptVersionPane extends KlVersionPane<ObservableConceptVersion> {
