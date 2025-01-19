40677a1e4adc63b126121cd7f691793d734bf9e9
Keith E Campbell <rotors.snooty-0e@icloud.com>
2025-01-19 09:23:05 -0800

    Tuning interfaces based on implementing corresponding blueprints.

--- knowledge-layout/src/main/java/dev/ikm/komet/layout/container/KlContainer.java
+++ knowledge-layout/src/main/java/dev/ikm/komet/layout/container/KlContainer.java
@@ -11,18 +11,6 @@ import java.util.List;
  * KlContainer is a class that implements the KlWidget interface and is responsible for
  * managing a collection of KlComponents within a GridPane layout.
  */
-public class KlContainer<T extends GridPane> implements KlWidget<T> {
-    private GridPane gridPane;
-    final List<KlComponentPane> klComponents = List.of();
-
-    /**
-     * Returns the GridPane instance managed by this KlContainer.
-     *
-     * @return the GridPane instance
-     */
-    @Override
-    public GridPane klWidget() {
-        return gridPane;
-    }
+public interface KlContainer<T extends GridPane> extends KlWidget<T> {
 
 }
