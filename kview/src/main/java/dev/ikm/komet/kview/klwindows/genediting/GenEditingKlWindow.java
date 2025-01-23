c24ceee2caa8a0d011580874d3c0f2df0f2baba2
Besmir Beqiri <bb@sandec.de>
2025-01-23 01:37:12 +0100

    Implement custom JavaFX control for `Infinite` Workspace by refactoring workspace related code from JournalController to enhance UI manageability and modularity. (#220)

--- kview/src/main/java/dev/ikm/komet/kview/klwindows/genediting/GenEditingKlWindow.java
+++ kview/src/main/java/dev/ikm/komet/kview/klwindows/genediting/GenEditingKlWindow.java
@@ -1,20 +1,12 @@
 package dev.ikm.komet.kview.klwindows.genediting;
 
-import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.CURRENT_JOURNAL_WINDOW_TOPIC;
-import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.VIEW_PROPERTIES;
-import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.FIELDS_COLLECTION;
-import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.REF_COMPONENT;
-import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.SEMANTIC;
-import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.WINDOW_TOPIC;
 import dev.ikm.komet.framework.view.ViewProperties;
-import dev.ikm.komet.kview.fxutils.window.WindowSupport;
 import dev.ikm.komet.kview.klwindows.AbstractEntityChapterKlWindow;
 import dev.ikm.komet.kview.mvvm.view.genediting.GenEditingDetailsController;
 import dev.ikm.komet.preferences.KometPreferences;
 import dev.ikm.tinkar.entity.EntityService;
 import dev.ikm.tinkar.entity.SemanticEntity;
 import dev.ikm.tinkar.terms.EntityFacade;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import org.carlfx.cognitive.loader.Config;
 import org.carlfx.cognitive.loader.FXMLMvvmLoader;
@@ -23,22 +15,30 @@ import org.carlfx.cognitive.loader.JFXNode;
 import java.util.ArrayList;
 import java.util.UUID;
 
+import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.CURRENT_JOURNAL_WINDOW_TOPIC;
+import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.VIEW_PROPERTIES;
+import static dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel.*;
+
 /**
  * The General Editing Chapter window showing semantic details on the Journal Window's surface as a JavaFX Pane
  * having the ability to resize, drag, close.
  */
 public class GenEditingKlWindow extends AbstractEntityChapterKlWindow {
+
+    /**
+     * Root container for the FXML UI and its controller.
+     */
     private JFXNode<Pane, GenEditingDetailsController> jfxNode;
 
     /**
+     * Constructs a new editing window for a specific semantic entity.
      *
-     * @param journalTopic
-     * @param entityFacade
-     * @param desktopSurface
-     * @param viewProperties
-     * @param preferences
+     * @param journalTopic   the UUID representing the journal topic the owning Journal Window uses to communicate events.
+     * @param entityFacade   entity facade when not null usually this will load and display the current details.
+     * @param viewProperties view properties is access to view calculators to query data.
+     * @param preferences    komet preferences assists on reading and writing data to preferences user.home/Solor/database_folder/preferences
      */
-    public GenEditingKlWindow(UUID journalTopic, EntityFacade entityFacade, Pane desktopSurface,  ViewProperties viewProperties, KometPreferences preferences) {
+    public GenEditingKlWindow(UUID journalTopic, EntityFacade entityFacade, ViewProperties viewProperties, KometPreferences preferences) {
         super(journalTopic, entityFacade, viewProperties, preferences);
 
         SemanticEntity entity = (SemanticEntity) EntityService.get().getEntity(entityFacade.nid()).get();
@@ -54,31 +54,26 @@ public class GenEditingKlWindow extends AbstractEntityChapterKlWindow {
                                 .setPropertyValue(REF_COMPONENT, refComponent)
                                 .setPropertyValue(SEMANTIC, entityFacade));
 
-        // create chapter window
+        // Create chapter window
         jfxNode = FXMLMvvmLoader.make(config);
 
-        //Getting the concept window pane
+        // Getting the concept window pane
         Pane chapterWindow = jfxNode.node();
-        WindowSupport windowSupport = new WindowSupport(chapterWindow, desktopSurface);
-        // Set the JavaFX Pane as the visible UI (chapter window)
-        setPaneWindow(chapterWindow);
 
-        //Calls the remove method to remove and concepts that were closed by the user.
+        // Set the JavaFX Pane as the visible UI (chapter window)
+        setRootPane(chapterWindow);
+
+        // Calls the remove method to remove and concepts that were closed by the user.
         jfxNode.controller().setOnCloseConceptWindow(windowEvent -> {
             getOnClose().ifPresent(Runnable::run);
             // TODO more clean up such as view models and listeners just in case (memory).
         });
-
     }
+
+    /**
+     * Called when the window is shown.
+     */
     public void onShown() {
         jfxNode.controller().putTitlePanesArrowOnRight();
-    }
-    @Override
-    public Pane getPaneWindow() {
-        return super.getPaneWindow();
-    }
-    @Override
-    public Scene scene() {
-        return null;
     }
 }
