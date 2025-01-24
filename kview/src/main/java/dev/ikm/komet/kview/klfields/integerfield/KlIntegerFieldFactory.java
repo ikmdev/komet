cd4763e0886b46253e366d0d511ed3239c9df240
Sten Anderson <sten_karl@yahoo.com>
2025-01-24 16:20:38 -0600

    added placeholder javadoc

--- kview/src/main/java/dev/ikm/komet/kview/klfields/integerfield/KlIntegerFieldFactory.java
+++ kview/src/main/java/dev/ikm/komet/kview/klfields/integerfield/KlIntegerFieldFactory.java
@@ -6,7 +6,10 @@ import dev.ikm.komet.layout.component.version.field.KlField;
 import dev.ikm.komet.layout.component.version.field.KlFieldFactory;
 import dev.ikm.komet.layout.component.version.field.KlIntegerField;
 
-public class KlIntegerFieldFactory  implements KlFieldFactory<Integer> {
+/**
+ * Placeholder Javadoc
+ */
+public class KlIntegerFieldFactory implements KlFieldFactory<Integer> {
 
     @Override
     public KlField<Integer> create(ObservableField<Integer> observableField, ObservableView observableView, boolean editable) {
