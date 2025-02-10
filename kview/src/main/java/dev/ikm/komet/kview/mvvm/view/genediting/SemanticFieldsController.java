030519d3416c353acf9b9b6c818c2f20e7dcd446
swsalvi <swaroopsalvi@gmail.com>
2025-02-10 16:44:18 -0500

    Some more fixes

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/genediting/SemanticFieldsController.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/genediting/SemanticFieldsController.java
@@ -26,9 +26,19 @@ import dev.ikm.komet.framework.observable.ObservableField;
 import dev.ikm.komet.framework.view.ViewProperties;
 import dev.ikm.komet.kview.events.genediting.GenEditingEvent;
 import dev.ikm.komet.kview.klfields.KlFieldHelper;
+import dev.ikm.tinkar.common.alert.AlertObject;
+import dev.ikm.tinkar.common.alert.AlertStreams;
+import dev.ikm.tinkar.common.service.TinkExecutor;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.coordinate.stamp.calculator.StampCalculator;
+import dev.ikm.tinkar.entity.Entity;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
+import dev.ikm.tinkar.entity.SemanticRecord;
+import dev.ikm.tinkar.entity.SemanticVersionRecord;
+import dev.ikm.tinkar.entity.StampEntity;
+import dev.ikm.tinkar.entity.StampRecord;
+import dev.ikm.tinkar.entity.transaction.CommitTransactionTask;
+import dev.ikm.tinkar.entity.transaction.Transaction;
 import dev.ikm.tinkar.terms.EntityFacade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -36,11 +46,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import org.carlfx.cognitive.loader.InjectViewModel;
 import org.carlfx.cognitive.viewmodel.ValidationViewModel;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SemanticFieldsController {
 
@@ -105,18 +119,74 @@ public class SemanticFieldsController {
     @FXML
     public void submit(ActionEvent actionEvent) {
         actionEvent.consume();
-        List<ObservableField<?>> list = new ArrayList<>();
-        observableFields.forEach(observableField -> {
-            if (observableField != null) {
-                runAsync(() -> {
-                    observableField.writeToDataBase();
-                });
+        //create new list to pass to to the event.
+        List<ObservableField<?>> list = new ArrayList<>(observableFields);
+
+        //Run async transaction to commit to DB.
+        runAsync(() -> {
+            Transaction transaction = writeToTempTranscation();
+            if(transaction !=null){
+                commitTransactionTask(transaction);
             }
-            list.add(observableField);
         });
+
+        //Get the semantic need to pass along with event for loading values across Opened Semantics.
         EntityFacade semantic = semanticFieldsViewModel.getPropertyValue(SEMANTIC);
         //EventBus implementation changes to refresh the details area
         EvtBusFactory.getDefaultEvtBus().publish(semanticFieldsViewModel.getPropertyValue(CURRENT_JOURNAL_WINDOW_TOPIC), new GenEditingEvent(actionEvent.getSource(), PUBLISH, list, semantic.nid()));
 
+    }
+
+    /**  This method is used to create and return transaction instance when records are modified.
+     * TODO If not data is modified and the transacti
+     * @return
+     */
+    private Transaction writeToTempTranscation() {
+        EntityFacade semantic = semanticFieldsViewModel.getPropertyValue(SEMANTIC);
+        SemanticRecord semanticRecord =  Entity.getFast(semantic.nid());
+        AtomicReference<Transaction> transactionAtomicReference = new AtomicReference<>();
+        StampCalculator stampCalculator = getViewProperties().calculator().stampCalculator();
+        Latest<SemanticEntityVersion> semanticEntityVersionLatest = stampCalculator.latest(semantic.nid());
+        semanticEntityVersionLatest.ifPresent(semanticEntityVersion ->{
+            StampRecord stamp = Entity.getStamp(semanticEntityVersion.stampNid());
+            SemanticVersionRecord version = Entity.getVersionFast(semantic.nid(), stamp.nid());
+            MutableList fieldsForNewVersion = Lists.mutable.of(version.fieldValues().toArray());
+            observableFields.forEach(of -> {
+                fieldsForNewVersion.set(of.fieldIndex(), of.value());
+            });
+            SemanticVersionRecord newVersion =null;
+            if(stamp.lastVersion().committed()){
+                // Create transaction
+                transactionAtomicReference.set(Transaction.make());
+                // newStamp already written to the entity store.
+                StampEntity newStamp = transactionAtomicReference.get().getStampForEntities(stamp.state(), stamp.authorNid(), stamp.moduleNid(), stamp.pathNid(), version.entity());
+                // Create new version...
+                newVersion = version.with().fieldValues(fieldsForNewVersion.toImmutable()).stampNid(newStamp.nid()).build();
+
+            }else {
+                newVersion = version.withFieldValues(fieldsForNewVersion.toImmutable());
+                // if a version with the same stamp as newVersion exists, that version will be removed
+            }
+
+            if(newVersion != null){
+                // prior to adding the new version so you don't get duplicate versions with the same stamp.
+                SemanticRecord analogue = semanticRecord.with(newVersion).build();
+                // Entity provider will broadcast the nid of the changed entity.
+                Entity.provider().putEntity(analogue);
+            }
+        });
+        return transactionAtomicReference.get();
+    }
+
+    private static void commitTransactionTask(Transaction transaction) {
+        CommitTransactionTask commitTransactionTask = new CommitTransactionTask(transaction);
+        Future<Void> future = TinkExecutor.threadPool().submit(commitTransactionTask);
+        TinkExecutor.threadPool().execute(() -> {
+            try {
+                future.get();
+            } catch (Exception e) {
+                AlertStreams.getRoot().dispatch(AlertObject.makeError(e));
+            }
+        });
     }
 }
