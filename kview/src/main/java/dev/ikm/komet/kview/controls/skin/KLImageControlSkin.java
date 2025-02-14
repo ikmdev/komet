3b33de40aec4cb95c78eac1e8d4966731251157e
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2025-02-14 11:48:55 +0000

    Feature/IIA-1446: restrict image file chooser to PNGs

--- kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLImageControlSkin.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLImageControlSkin.java
@@ -116,7 +116,8 @@ public class KLImageControlSkin extends SkinBase<KLImageControl> {
 
         // Set extension filters to only show image files
         fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+//                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+                new FileChooser.ExtensionFilter("Image Files", "*.png")
         );
 
         // Show open file dialog
