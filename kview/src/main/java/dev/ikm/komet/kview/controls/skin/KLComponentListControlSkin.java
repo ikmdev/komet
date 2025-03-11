02c6f7d97f7de4887a23ced4b79683008cadd7cd
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2025-03-11 22:18:06 +0000

    Feature/IIA-1594: Component List Control drag and drop to reorder no longer works when dropping to a drop line

--- kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLComponentListControlSkin.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLComponentListControlSkin.java
@@ -111,7 +111,7 @@ public class KLComponentListControlSkin extends SkinBase<KLComponentListControl>
 
     private void onDragOver(DragEvent dragEvent) {
         if (dragEvent.getDragboard().hasContent(KLComponentControlSkin.COMPONENT_CONTROL_DRAG_FORMAT)) {
-            dragEvent.acceptTransferModes(TransferMode.COPY);
+            dragEvent.acceptTransferModes(TransferMode.MOVE);
         }
 
         updateDropTargetLocation(dragEvent);
