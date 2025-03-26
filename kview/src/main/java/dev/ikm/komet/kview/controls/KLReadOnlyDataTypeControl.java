7edc023ea921a57ab492fb78398ff236556fb251
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2025-03-26 16:49:03 +0000

    Feature/finished/iia-1467 iia-1468 iia-1469 iia-1670 placeholder (#340)
    * Update version to start feature: IIA-1467-IIA-1468-IIA-1469-IIA-1670-Placeholder
    
    * Feature/IIA-1467, IIA-1468, IIA-1469, IIA-1470: Add placeholder feature to data types
    
    * Update version to finish feature: IIA-1467-IIA-1468-IIA-1469-IIA-1670-Placeholder
    
    * Feature/IIA-1467, IIA-1468, IIA-1469, IIA-1470: remove commented out code
    
    ---------
    
    Co-authored-by: Doug Holubek <dholubek@users.noreply.github.com>

--- kview/src/main/java/dev/ikm/komet/kview/controls/KLReadOnlyDataTypeControl.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/KLReadOnlyDataTypeControl.java
@@ -5,23 +5,48 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.control.Skin;
 
-public class KLReadOnlyDataTypeControl<T> extends KLReadOnlyBaseControl {
+public class KLReadOnlyDataTypeControl<T> extends KLReadOnlyBaseControl<T> {
 
     public KLReadOnlyDataTypeControl() {
-        getStyleClass().add("read-only-string-control");
+        getStyleClass().add("read-only-data-control");
     }
 
-    // -- value
-    private ObjectProperty<T> value = new SimpleObjectProperty<>();
-    public T getValue() { return value.get(); }
-    public ObjectProperty<T> valueProperty() { return value; }
-    public void setValue(T value) { this.value.set(value); }
+    public KLReadOnlyDataTypeControl(Class<T> type) {
+        this();
+        this.classDataType = type;
+    }
 
     // -- on add units of measure action
     private ObjectProperty<Runnable> onAddUnitsOfMeasureAction = new SimpleObjectProperty<>();
     public Runnable getOnAddUnitsOfMeasureAction() { return onAddUnitsOfMeasureAction.get(); }
     public ObjectProperty<Runnable> onAddUnitsOfMeasureActionProperty() { return onAddUnitsOfMeasureAction; }
     public void setOnAddUnitsOfMeasureAction(Runnable onAddUnitsOfMeasureAction) { this.onAddUnitsOfMeasureAction.set(onAddUnitsOfMeasureAction); }
+
+    // -- class data type
+    private Class<T> classDataType;
+
+    /**
+     * The type of the data being stored, which is the type of the value property.
+     * This is also used to display different contextmenus depending on the type of the
+     * value property.
+     *
+     * @return The type of the data being stored, which is the type of the value property.
+     */
+    public Class<T> getClassDataType() { return classDataType; }
+
+    // -- data type
+    private String dataType;
+    public String getDataType() { return dataType; }
+    /**
+     * This is another property and another way of specifying the data being stored, which is the
+     * type of the value property.
+     * This should be mostly used in fxml as in fxml you can't specify generic information or
+     * set Class types.
+     */
+    public void setDataType(String value) throws ClassNotFoundException {
+        classDataType = (Class<T>) Class.forName(value);
+        this.dataType = value;
+    }
 
     @Override
     protected Skin<?> createDefaultSkin() {
@@ -30,6 +55,6 @@ public class KLReadOnlyDataTypeControl<T> extends KLReadOnlyBaseControl {
 
     @Override
     public String getUserAgentStylesheet() {
-        return KLInstantControl.class.getResource("read-only-string-control.css").toExternalForm();
+        return KLInstantControl.class.getResource("read-only-data-control.css").toExternalForm();
     }
 }
