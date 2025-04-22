a9a5573be9aefcb519f7b7bc27f1f242b07914a3
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2025-04-22 12:51:18 +0100

    Feature/IIA-1844: Component List and Component Set not being highlighted when on edit mode

--- kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLReadOnlyMultiComponentControlSkin.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLReadOnlyMultiComponentControlSkin.java
@@ -16,6 +16,8 @@ import javafx.scene.layout.VBox;
 
 import java.util.HashMap;
 
+import static dev.ikm.komet.kview.controls.skin.KLReadOnlyBaseControlSkin.EDIT_MODE_PSEUDO_CLASS;
+
 public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMultiComponentControl> extends SkinBase<C> {
     private final VBox mainContainer = new VBox();
 
@@ -26,6 +28,8 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
     protected final VBox componentsContainer = new VBox();
 
     protected final HashMap<ComponentItem, Node> componentUIItems = new HashMap<>();
+
+    private boolean wasEditActionFired = false;
 
     /**
      * @param control The control for which this Skin should attach to.
@@ -67,6 +71,8 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
         promptTextLabel.setContextMenu(promptTextContextMenu);
         promptTextContextMenu.setOnShown(value -> onContextMenuForPromptShown(promptTextContextMenu));
 
+        control.editModeProperty().subscribe(this::onEditModeChanged);
+
         // CSS
         mainContainer.getStyleClass().add("main-container");
         componentsContainer.getStyleClass().add("components-container");
@@ -91,7 +97,9 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
         componentsContainer.setManaged(!promptTextVisible);
     }
 
-    protected ContextMenu createContextMenu(ComponentItem componentItem) {
+    protected final ContextMenu createContextMenu(ComponentItem componentItem) {
+        KLReadOnlyMultiComponentControl control = getSkinnable();
+
         ContextMenu contextMenu = new ContextMenu();
 
         contextMenu.getStyleClass().add("klcontext-menu");
@@ -110,6 +118,14 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
         if (componentItem == null) {
             removeMenuItem.setDisable(true);
         }
+
+        contextMenu.showingProperty().addListener(observable -> {
+            if (!contextMenu.isShowing() && !wasEditActionFired) {
+                control.setEditMode(false);
+            } else if (!contextMenu.isShowing() && wasEditActionFired){
+                control.setEditMode(true);
+            }
+        });
 
         return contextMenu;
     }
@@ -131,6 +147,7 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
 
     protected void fireOnEditAction(ActionEvent actionEvent) {
         if (getSkinnable().getOnEditAction() != null) {
+            wasEditActionFired = true;
             getSkinnable().getOnEditAction().run();
         }
     }
@@ -139,5 +156,10 @@ public abstract class KLReadOnlyMultiComponentControlSkin<C extends KLReadOnlyMu
         if (getSkinnable().getOnRemoveAction() != null) {
             getSkinnable().getOnRemoveAction().accept(componentItem);
         }
+    }
+
+    private void onEditModeChanged() {
+        pseudoClassStateChanged(EDIT_MODE_PSEUDO_CLASS, getSkinnable().isEditMode());
+        wasEditActionFired = false;
     }
 }
