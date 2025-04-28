2e167c20bb6a80a11987d21506dc193a8a531ada
Doug Holubek <dholubek@deloitte.com>
2025-04-28 16:05:00 -0400

    working with more explicit listener code

--- kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLReadOnlyComponentControlSkin.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/skin/KLReadOnlyComponentControlSkin.java
@@ -3,6 +3,8 @@ package dev.ikm.komet.kview.controls.skin;
 import dev.ikm.komet.kview.NodeUtils;
 import dev.ikm.komet.kview.controls.*;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.Label;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.image.ImageView;
@@ -25,8 +27,19 @@ public class KLReadOnlyComponentControlSkin extends KLReadOnlyBaseControlSkin<KL
 
         textContainer.getChildren().addAll(iconImageView, promptTextLabel, textLabel);
 
-        textLabel.textProperty().bind(Bindings.select(control, "value", "text"));
-        iconImageView.imageProperty().bind(Bindings.select(control, "value", "icon"));
+        if (control.getValue() != null) {
+            textLabel.setText(control.getValue().getText());
+            iconImageView.setImage(control.getValue().getIcon());
+        }
+        control.valueProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal != null) {
+                textLabel.setText(newVal.getText());
+                iconImageView.setImage(newVal.getIcon());
+            } else {
+                textLabel.setText("");
+                iconImageView.setImage(null);
+            }
+        });
 
         textLabel.setMaxWidth(Double.MAX_VALUE);
         HBox.setHgrow(textLabel, Priority.ALWAYS);
