dbb590c4a36d078b6456add106853f0a96f00281
swsalvi <swaroopsalvi@gmail.com>
2025-05-07 16:16:55 -0400

    Added logic to debounce for 3 secs.

--- kview/src/main/java/dev/ikm/komet/kview/klfields/floatfield/DefaultKlFloatField.java
+++ kview/src/main/java/dev/ikm/komet/kview/klfields/floatfield/DefaultKlFloatField.java
@@ -1,11 +1,13 @@
 package dev.ikm.komet.kview.klfields.floatfield;
 
+import static dev.ikm.komet.kview.klfields.KlFieldHelper.createTimeline;
 import dev.ikm.komet.framework.observable.ObservableField;
 import dev.ikm.komet.framework.view.ObservableView;
 import dev.ikm.komet.kview.controls.KLFloatControl;
 import dev.ikm.komet.kview.controls.KLReadOnlyDataTypeControl;
 import dev.ikm.komet.kview.klfields.BaseDefaultKlField;
 import dev.ikm.komet.layout.component.version.field.KlFloatField;
+import javafx.animation.Timeline;
 import javafx.scene.Parent;
 
 public class DefaultKlFloatField extends BaseDefaultKlField<Float> implements KlFloatField {
@@ -16,10 +18,15 @@ public class DefaultKlFloatField extends BaseDefaultKlField<Float> implements Kl
         Parent node;
         if (isEditable) {
             KLFloatControl floatControl = new KLFloatControl();
-
-            floatControl.valueProperty().bindBidirectional(observableFloatField.valueProperty());
             floatControl.setTitle(getTitle());
-
+            floatControl.valueProperty().set(observableFloatField.valueProperty().getValue());
+            Timeline timeline = createTimeline();
+            timeline.setOnFinished((evt) -> {
+                observableFloatField.valueProperty().setValue(floatControl.getValue());
+            });
+            floatControl.valueProperty().subscribe( s -> {
+                timeline.playFromStart();
+            });
             node = floatControl;
         } else {
             KLReadOnlyDataTypeControl<Float> readOnlyStringControl = new KLReadOnlyDataTypeControl<>(Float.class);
--- kview/src/main/java/dev/ikm/komet/kview/klfields/stringfield/DefaultKlStringField.java
+++ kview/src/main/java/dev/ikm/komet/kview/klfields/stringfield/DefaultKlStringField.java
@@ -1,11 +1,13 @@
 package dev.ikm.komet.kview.klfields.stringfield;
 
+import static dev.ikm.komet.kview.klfields.KlFieldHelper.createTimeline;
 import dev.ikm.komet.framework.observable.ObservableField;
 import dev.ikm.komet.framework.view.ObservableView;
 import dev.ikm.komet.kview.controls.KLReadOnlyDataTypeControl;
 import dev.ikm.komet.kview.controls.KLStringControl;
 import dev.ikm.komet.kview.klfields.BaseDefaultKlField;
 import dev.ikm.komet.layout.component.version.field.KlStringField;
+import javafx.animation.Timeline;
 import javafx.scene.Parent;
 
 public class DefaultKlStringField extends BaseDefaultKlField<String> implements KlStringField {
@@ -16,10 +18,15 @@ public class DefaultKlStringField extends BaseDefaultKlField<String> implements 
         Parent node;
         if (isEditable) {
             KLStringControl stringControl = new KLStringControl();
-
-            stringControl.textProperty().bindBidirectional(observableStringField.valueProperty());
             stringControl.setTitle(getTitle());
-
+            stringControl.textProperty().set(observableStringField.valueProperty().getValue());
+            Timeline timeline = createTimeline();
+            timeline.setOnFinished((evt) -> {
+                observableStringField.valueProperty().setValue(stringControl.getText());
+            });
+            stringControl.textProperty().subscribe( s -> {
+                timeline.playFromStart();
+            });
             node = stringControl;
         } else {
             KLReadOnlyDataTypeControl<String> readOnlyStringControl = new KLReadOnlyDataTypeControl<>(String.class);
