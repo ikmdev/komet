a154a855132e5d5ca979ae4213cfe381d8a9a0eb
bhharsh13 <bhavani.sreehari@gmail.com>
2025-05-09 14:17:22 -0700

    IIA-1908 Fixed the issue for Newly Imported Pattern display in Pattern Navigator (#429)
    * Update version to start feature: IIA-1908-pattern-navigator-import
    
    * Fixed the issue for Newly Imported Pattern display in Pattern Navigator
    
    * Update version to finish feature: IIA-1908-pattern-navigator-import
    
    * Newly Imported Pattern display in Pattern Navigator
    
    * changed to use FormViewModel create
    
    * removed the duplicate LoadEntitiesFromProtobufFile
    
    * change in if condition check to use CREATE
    
    * removed unused import
    
    * Fixed the PatternSavedEvent

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/genediting/SemanticFieldsController.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/genediting/SemanticFieldsController.java
@@ -20,7 +20,6 @@ import static dev.ikm.komet.framework.events.FrameworkTopics.VERSION_CHANGED_TOP
 import static dev.ikm.komet.kview.events.EventTopics.SAVE_PATTERN_TOPIC;
 import static dev.ikm.komet.kview.events.genediting.GenEditingEvent.PUBLISH;
 import static dev.ikm.komet.kview.events.genediting.PropertyPanelEvent.CLOSE_PANEL;
-import static dev.ikm.komet.kview.events.pattern.PatternCreationEvent.PATTERN_CREATION_EVENT;
 import static dev.ikm.komet.kview.klfields.KlFieldHelper.calculteHashValue;
 import static dev.ikm.komet.kview.klfields.KlFieldHelper.createDefaultFieldValues;
 import static dev.ikm.komet.kview.klfields.KlFieldHelper.generateNode;
@@ -45,7 +44,7 @@ import dev.ikm.komet.framework.observable.ObservableSemanticVersion;
 import dev.ikm.komet.framework.view.ViewProperties;
 import dev.ikm.komet.kview.events.genediting.GenEditingEvent;
 import dev.ikm.komet.kview.events.genediting.PropertyPanelEvent;
-import dev.ikm.komet.kview.events.pattern.PatternCreationEvent;
+import dev.ikm.komet.kview.events.pattern.PatternSavedEvent;
 import dev.ikm.komet.kview.mvvm.viewmodel.GenEditingViewModel;
 import dev.ikm.tinkar.common.service.TinkExecutor;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
@@ -217,7 +216,7 @@ public class SemanticFieldsController {
             if(reloadPatternNavigator && genEditingViewModel.getPropertyValue(MODE) == CREATE) {
                 // refresh the pattern navigation
                 EvtBusFactory.getDefaultEvtBus().publish(SAVE_PATTERN_TOPIC,
-                        new PatternCreationEvent(this, PATTERN_CREATION_EVENT));
+                        new PatternSavedEvent(this, PatternSavedEvent.PATTERN_CREATION_EVENT));
                 reloadPatternNavigator = false;
             }
             enableDisableSubmitButton();
@@ -346,7 +345,7 @@ public class SemanticFieldsController {
                        new GenEditingEvent(actionEvent.getSource(), PUBLISH, list, semantic.nid()));
                // refesh the pattern navigation
                EvtBusFactory.getDefaultEvtBus().publish(SAVE_PATTERN_TOPIC,
-                       new PatternCreationEvent(actionEvent.getSource(), PATTERN_CREATION_EVENT));
+                       new PatternSavedEvent(actionEvent.getSource(), PatternSavedEvent.PATTERN_UPDATE_EVENT));
            }, () -> {
                //TODO this is a temp alert / workaround till we figure how to reload transactions across multiple restarts of app.
                LOG.error("Unable to commit: Transaction for the given version does not exist.");
