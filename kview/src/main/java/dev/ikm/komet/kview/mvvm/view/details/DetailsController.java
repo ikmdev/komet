5a50b80493fd2d4f52a06500f15fc5eff0336fbf
bhharsh13 <bhavani.sreehari@gmail.com>
2025-06-10 13:23:27 -0700

    reverted updateConceptBanner(DescrName) code

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/details/DetailsController.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/details/DetailsController.java
@@ -893,10 +893,8 @@ public class DetailsController  {
     public void updateConceptBanner(DescrName fqnDescrName) {
         if (fqnDescrName == null) return;
 
-        EntityFacade entityFacade = conceptViewModel.getPropertyValue(CURRENT_ENTITY);
         // Title (FQN of concept)
-        final ViewCalculator viewCalculator = viewProperties.calculator();
-        String conceptNameStr = viewCalculator.getFullyQualifiedDescriptionTextWithFallbackOrNid(entityFacade.nid());
+        String conceptNameStr = fqnDescrName.getNameText();
         fqnTitleText.setText(conceptNameStr);
         conceptNameTooltip.setText(conceptNameStr);
 
