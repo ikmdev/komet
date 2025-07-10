c8992bbfe353cff12e1142c7445cc436b69014de
swsalvi <swsalvi@deloitte.com>
2025-07-10 16:50:11 -0400

    Passed journalTopic to limit event in Journal Window.

--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/search/SearchCellDescriptionSemantic.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/search/SearchCellDescriptionSemantic.java
@@ -2,20 +2,27 @@ package dev.ikm.komet.kview.mvvm.view.search;
 
 import dev.ikm.komet.framework.Identicon;
 import dev.ikm.komet.framework.view.ObservableViewNoOverride;
+import dev.ikm.komet.framework.view.ViewProperties;
 import dev.ikm.tinkar.coordinate.stamp.calculator.LatestVersionSearchResult;
 import dev.ikm.tinkar.entity.ConceptEntity;
 import dev.ikm.tinkar.entity.Entity;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import org.carlfx.cognitive.loader.Config;
 import org.carlfx.cognitive.loader.FXMLMvvmLoader;
 import org.carlfx.cognitive.loader.JFXNode;
 
 import static dev.ikm.komet.kview.mvvm.view.search.NextGenSearchController.getDragAndDropType;
 import static dev.ikm.komet.kview.mvvm.view.search.NextGenSearchController.setUpDraggable;
+import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.CURRENT_JOURNAL_WINDOW_TOPIC;
+import static dev.ikm.komet.kview.mvvm.viewmodel.FormViewModel.VIEW_PROPERTIES;
+
+import java.util.UUID;
 
 // Unfortunately we cannot have the generic type defined in the ListCell (the item needs to be of type Object).
 // There seems to be a bug in JavaFX where after you change the Cell Factory, the now defunct Cells still hang
@@ -27,15 +34,24 @@ public class SearchCellDescriptionSemantic extends ListCell {
 
     private SortResultSemanticEntryController controller;
     private Node content;
+    private final ViewProperties viewProperties;
     private ObservableViewNoOverride observableViewNoOverride;
 
-    public SearchCellDescriptionSemantic(ObservableViewNoOverride observableViewNoOverride) {
-        JFXNode<Pane, SortResultSemanticEntryController> searchSemanticEntryJFXNode = FXMLMvvmLoader
-                .make(SortResultSemanticEntryController.class.getResource(SORT_SEMANTIC_RESULT_CONCEPT_FXML));
+    public SearchCellDescriptionSemantic(ViewProperties viewProperties, UUID journalTopic, ObservableViewNoOverride observableViewNoOverride) {
 
+        this.viewProperties = viewProperties;
         this.observableViewNoOverride = observableViewNoOverride;
+        Config config = new Config(SortResultSemanticEntryController.class.getResource(SORT_SEMANTIC_RESULT_CONCEPT_FXML));
+        config.updateViewModel("searchEntryViewModel", (searchEntryViewModel) ->
+                searchEntryViewModel
+                        .addProperty(VIEW_PROPERTIES, viewProperties)
+                        .addProperty(CURRENT_JOURNAL_WINDOW_TOPIC, journalTopic)
+        );
+        JFXNode<Pane, SortResultSemanticEntryController> searchSemanticEntryJFXNode = FXMLMvvmLoader.make(config);
         content = searchSemanticEntryJFXNode.node();
         controller = searchSemanticEntryJFXNode.controller();
+        setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+
     }
 
     @Override
