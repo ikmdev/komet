1d0456de232699da8e83780516de1b684bfc0446
Pedro Duque Vieira <pedro.duquevieira@gmail.com>
2025-07-13 13:50:01 +0100

    Feature/IIA-2384: Add support for Component, CSet and CList and fix visual glitches

--- kview/src/main/java/dev/ikm/komet/kview/controls/skin/ComponentItemNode.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/skin/ComponentItemNode.java
@@ -2,6 +2,8 @@ package dev.ikm.komet.kview.controls.skin;
 
 import dev.ikm.komet.kview.controls.ComponentItem;
 import dev.ikm.komet.kview.controls.KLReadOnlyMultiComponentControl;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -10,6 +12,7 @@ import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.shape.Circle;
 
 /**
  * A Node used to render a Component (icon + text)
@@ -18,6 +21,8 @@ public class ComponentItemNode extends Region {
     private final HBox container = new HBox();
     private final ImageView iconImageView = new ImageView();
     private final Label textLabel = new Label();
+
+    private Circle circleClip;
 
     private ContextMenu contextMenu;
 
@@ -30,6 +35,11 @@ public class ComponentItemNode extends Region {
         iconImageView.imageProperty().bind(componentItem.iconProperty());
         iconImageView.setFitHeight(16);
         iconImageView.setFitWidth(16);
+
+        // Clip for circled image mode
+        circleClip = new Circle(8);
+        circleClip.setCenterX(8);
+        circleClip.setCenterY(8);
 
         // Label (Text)
         textLabel.textProperty().bind(componentItem.textProperty());
@@ -46,6 +56,22 @@ public class ComponentItemNode extends Region {
         getStyleClass().add("component-item");
         container.getStyleClass().add("container");
     }
+
+    // -- circular
+    BooleanProperty circular = new SimpleBooleanProperty(false) {
+        @Override
+        protected void invalidated() {
+            if (get()) {
+                iconImageView.setClip(circleClip);
+            } else {
+                iconImageView.setClip(null);
+            }
+        }
+    };
+    public boolean isCircular() { return circular.get(); }
+    public BooleanProperty circularProperty() { return circular; }
+    public void setCircular(boolean circular) { this.circular.set(circular); }
+
 
     void setContextMenu(ContextMenu contextMenu) {
         this.contextMenu = contextMenu;
--- kview/src/main/java/dev/ikm/komet/kview/mvvm/view/navigation/SemanticTooltip.java
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/navigation/SemanticTooltip.java
@@ -1,14 +1,19 @@
 package dev.ikm.komet.kview.mvvm.view.navigation;
 
+import dev.ikm.komet.framework.Identicon;
 import dev.ikm.komet.framework.view.ViewProperties;
+import dev.ikm.komet.kview.controls.skin.ComponentItemNode;
 import dev.ikm.komet.kview.klfields.KlFieldHelper;
 import dev.ikm.komet.kview.klfields.KlFieldType;
+import dev.ikm.tinkar.common.id.IntIdCollection;
 import dev.ikm.tinkar.coordinate.stamp.calculator.Latest;
 import dev.ikm.tinkar.entity.PatternVersionRecord;
 import dev.ikm.tinkar.entity.SemanticEntity;
 import dev.ikm.tinkar.entity.SemanticEntityVersion;
+import dev.ikm.tinkar.terms.EntityProxy;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.Tooltip;
@@ -16,9 +21,11 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import org.eclipse.collections.api.list.ImmutableList;
 
@@ -50,7 +57,7 @@ public class SemanticTooltip extends Tooltip {
 
         setGraphic(mainContainer);
 
-        setShowDuration(Duration.seconds(10));
+        setShowDuration(Duration.seconds(15));
 
         // CSS
         getStyleClass().add("semantic-tooltip");
@@ -67,6 +74,7 @@ public class SemanticTooltip extends Tooltip {
         ColumnConstraints column2 = new ColumnConstraints();
         column2.setPercentWidth(50);
         column2.setHalignment(HPos.RIGHT);
+        column2.setFillWidth(false);
 
         grid.getColumnConstraints().addAll(column1, column2);
     }
@@ -114,41 +122,97 @@ public class SemanticTooltip extends Tooltip {
 
         optionalFieldType.ifPresent(fieldType -> {
             Object field = fields.get(fieldIndex);
+            Node nodeToAdd = null;
+
             switch(fieldType) {
                 case STRING, INTEGER, FLOAT, BOOLEAN -> {
-                    String fieldValue = field.toString();
-
-                    Label valueLabel = new Label(fieldValue);
-                    valueLabel.getStyleClass().add("field-value");
-                    grid.add(valueLabel, 1, rowIndex);
+                    nodeToAdd = createSimpleFieldValue(field);
                 }
                 case IMAGE -> {
-
-                    Image image = KlFieldHelper.newImageFromByteArray((byte[])field);
-
-                    ImageView imagePreview = new ImageView(image);
-                    imagePreview.setPreserveRatio(true);
-                    imagePreview.getStyleClass().add("field-value");
-
-                    if (image.getWidth() > 150) {
-                        imagePreview.setFitWidth(150);
-                    }
-                    if (image.getHeight() > 150) {
-                        imagePreview.setFitHeight(150);
-                    }
-
-                    BorderPane imageContainer = new BorderPane();
-                    imageContainer.setRight(imagePreview);
-
-                    imageContainer.getStyleClass().add("image-container");
-
-                    grid.add(imageContainer, 1, rowIndex);
+                    nodeToAdd = createImageFieldValue((byte[]) field);
                 }
+
+                case COMPONENT -> {
+                    nodeToAdd = createComponentFieldValue((EntityProxy) field);
+                }
+                case C_LIST, C_SET -> {
+                    nodeToAdd = createCListOrCSetFieldValue((IntIdCollection) field);
+                }
+            }
+
+            if (nodeToAdd != null) {
+                grid.add(nodeToAdd, 1, rowIndex);
             }
         });
 
         RowConstraints rowConstraints = new RowConstraints();
         rowConstraints.setValignment(VPos.TOP);
         grid.getRowConstraints().add(rowConstraints);
+    }
+
+    private FlowPane createCListOrCSetFieldValue(IntIdCollection field) {
+        FlowPane flowPane = new FlowPane();
+
+        IntIdCollection intIdCollection = field;
+        intIdCollection.forEach(nid -> {
+            EntityProxy entityProxy = EntityProxy.make(nid);
+            ComponentItemNode componentItemNode = createComponentItemNode(entityProxy);
+            flowPane.getChildren().add(componentItemNode);
+        });
+
+        flowPane.getStyleClass().add("components-container");
+        return flowPane;
+    }
+
+    private ComponentItemNode createComponentFieldValue(EntityProxy field) {
+        ComponentItemNode componentItemNode = createComponentItemNode(field);
+
+        componentItemNode.getStyleClass().add("single-component");
+        return componentItemNode;
+    }
+
+    private BorderPane createImageFieldValue(byte[] field) {
+        Image image = KlFieldHelper.newImageFromByteArray(field);
+
+        ImageView imagePreview = new ImageView(image);
+        imagePreview.setPreserveRatio(true);
+        imagePreview.getStyleClass().add("field-value");
+
+        if (image.getWidth() > 200) {
+            imagePreview.setFitWidth(200);
+        }
+        if (image.getHeight() > 200) {
+            imagePreview.setFitHeight(200);
+        }
+
+        Rectangle rectangle = new Rectangle(imagePreview.getBoundsInLocal().getWidth(), imagePreview.getBoundsInLocal().getHeight());
+        rectangle.setArcHeight(15);
+        rectangle.setArcWidth(15);
+        imagePreview.setClip(rectangle);
+
+        BorderPane imageContainer = new BorderPane();
+        imageContainer.setRight(imagePreview);
+
+        imageContainer.getStyleClass().add("image-container");
+        return imageContainer;
+    }
+
+    private Label createSimpleFieldValue(Object field) {
+        String fieldValue = field.toString();
+
+        Label valueLabel = new Label(fieldValue);
+        valueLabel.getStyleClass().add("field-value");
+        return valueLabel;
+    }
+
+    private ComponentItemNode createComponentItemNode(EntityProxy entityProxy) {
+        String description = viewProperties.calculator().languageCalculator()
+                .getFullyQualifiedDescriptionTextWithFallbackOrNid(entityProxy.nid());
+        Image identicon = Identicon.generateIdenticonImage(entityProxy.publicId());
+
+        ComponentItemNode componentItemNode = new ComponentItemNode(description, identicon);
+        componentItemNode.setCircular(true);
+
+        return componentItemNode;
     }
 }
