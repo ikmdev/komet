1c42c4e691d14c1f15c9c3e8d38aa9e4366ded3e
thorstenstueker <78673100+thorstenstueker@users.noreply.github.com>
2025-08-09 12:35:14 +0200

    implemented loginuserlist

---
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/loginuserlist/LoginUserListUsermodel.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class LoginUserListUsermodel {
+
+    public String username;
+    public String userpassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpassword() {
+        return userpassword;
+    }
+
+    public void setUserpassword(String userpassword) {
+        this.userpassword = userpassword;
+    }
+}
---
+++ kview/src/main/java/dev/ikm/komet/kview/mvvm/view/loginuserlist/LoginUserlistController.java
@@ -0,0 +1,190 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginUserlistController {
+    private static boolean isvisible = false;
+
+    @FXML
+    private PasswordField passwordfield;
+    @FXML
+    private ComboBox userchooser;
+    @FXML
+    private Button LoginButton;
+    @FXML
+    private Label passworderrorlabel;
+    @FXML
+    private Label usererrorlabel;
+    @FXML
+    private ImageView visibilityicon;
+    @FXML
+    private TextField passwordtextfield;
+    @FXML
+    private AnchorPane MainPane;
+    @FXML
+    private Label loginerrorlabel;
+
+    private boolean Loginenabled=false;
+
+    @FXML
+    public void initialize() {
+
+
+
+        /// with this call we produce the fake users List and the fake passwords List. You need to implement your dataprovider instead of this to have a
+// real login functionality. This is just a mockup for later use.
+        LoginUserListdatamodel.fakeusers();
+
+
+        userchooser.setPromptText("Select a user");
+        userchooser.setItems(LoginUserListdatamodel.list);
+        passwordtextfield.setVisible(false);
+LoginButton.setDisable(true);
+
+        System.out.println("Initializing");
+
+    }
+
+    @FXML
+    public void SignupAction(ActionEvent actionEvent) {
+
+        if (isvisible) {
+            SwapVisibility();
+        }
+
+
+        System.out.println("Signup");
+        boolean error = false;
+        String Errormessage = "";
+        boolean loginproceed = false;
+
+
+        System.out.println(userchooser.getValue());
+        if (userchooser.getValue() == null) {
+
+            error = true;
+            Errormessage = "Error: " + "Please select a User";
+            usererrorlabel.setText(Errormessage);
+
+        } else {
+
+            String username = userchooser.getValue().toString();
+            String password = "";
+            if (!isvisible) {
+                password = passwordfield.getText();
+            } else {
+                password = passwordtextfield.getText();
+            }
+            usererrorlabel.setText("");
+            if (password.length() > 4) {
+
+                loginproceed = true;
+                passworderrorlabel.setText("");
+            } else {
+
+                error = true;
+                Errormessage = "ERROR: Password must be at least 5 characters long";
+                passworderrorlabel.setText(Errormessage);
+                passworderrorlabel.setVisible(true);
+            }
+
+        }
+        if (loginproceed) {
+
+            if (isvisible) {
+                //visibleclicked(null);
+            }
+
+            boolean valid = LoginUserListdatamodel.validateUser(userchooser.getValue().toString(), passwordfield.getText());
+            if (valid) {
+
+                loginerrorlabel.setText("");
+
+                // Here you can add the code to proceed with the login, e.g. opening a new window or changing the scene
+
+                //Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                //stage.close();
+
+                System.out.println("Login Successful");
+            } else {
+
+                loginerrorlabel.setText("Login failed, please check your credentials");
+
+            }
+        }
+
+
+    }
+
+    @FXML
+    public void cleanerror(ActionEvent actionEvent) {
+
+        cleanerrorlabels();
+    }
+
+    public void cleanerrorlabels() {
+        //this method is used to clean the error labels, when the user clicks on the textfield or passwordfield
+        usererrorlabel.setText("");
+        passworderrorlabel.setText("");
+        loginerrorlabel.setText("");
+    }
+
+    @FXML
+    public void countlength(Event event) {
+        clicked();//key typed in, counts before the adding to the string
+
+    }
+
+    @FXML
+    public void clicked() {
+        cleanerrorlabels();
+        //vslidator for the password field, checks if the password is at least 5 characters long in visible and invisible mode
+        String pwvalidate = "";
+        if (!isvisible) {
+            pwvalidate = passwordfield.getText();
+        } else {
+            pwvalidate = passwordtextfield.getText();
+        }
+        System.out.println(passwordfield.getText().length());
+        if (pwvalidate.length() >= 4) {
+            passworderrorlabel.setText("");
+            LoginButton.setDisable(false);
+        }
+    }
+
+    @FXML
+    public void visibleclicked(Event event) {
+        SwapVisibility();
+        cleanerrorlabels();
+    }
+
+    public void SwapVisibility() {
+        //this method swaps the visibility of the password field and the password textfield for editing the password and switching invisible when login is clicked
+        //it also swaps the visibility icon from hidden to visible and vice versa
+        //this is used to show the password in plain text or as a password field with masked text
+        if (!isvisible) {
+            visibilityicon.setImage(new javafx.scene.image.Image("view.png"));
+            String a = passwordfield.getText();
+            passwordfield.setVisible(false);
+            passwordtextfield.setText(a);
+            passwordtextfield.setPromptText("Password");
+            passwordtextfield.setVisible(true);
+            isvisible = true;
+        } else {
+            visibilityicon.setImage(new javafx.scene.image.Image("hidden.png"));
+            String a = passwordtextfield.getText();
+            passwordtextfield.setVisible(false);
+            passwordfield.setText(a);
+            passwordfield.setVisible(true);
+            isvisible = false;
+        }
+
+    }
+
+
+}
