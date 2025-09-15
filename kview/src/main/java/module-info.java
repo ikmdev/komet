83289542920984c4704f9e127c3f2dff03a95136
thorstenstueker <78673100+thorstenstueker@users.noreply.github.com>
2025-09-15 17:51:27 +0200

    change JPRO-191

--- kview/src/main/java/module-info.java
+++ kview/src/main/java/module-info.java
@@ -38,6 +38,7 @@ module dev.ikm.komet.kview {
     requires org.eclipse.collections.api;
     requires org.eclipse.collections.impl;
     requires javafx.graphics;
+    requires org.apache.commons.logging;
 
     exports dev.ikm.komet.kview.state;
     exports dev.ikm.komet.kview.state.pattern;
