2041c440b340f2f383bf18645959a8e79fd80ad6
jdsmithsos <jdsmith@smithoptimalsolutions.com>
2025-09-16 09:12:07 -0600

    feature/finished/IIA-2605-2733-rework-to-copy-only-identifer (#761)
    * IIA-2605 IIA-2733 added property to trim the identifier string to remove the prefix
    
    * IIA-2605 made property types more abstract
    
    ---------
    
    Co-authored-by: Carl Dea <carl.dea@gmail.com>

--- kview/src/main/java/dev/ikm/komet/kview/controls/PublicIDControl.java
+++ kview/src/main/java/dev/ikm/komet/kview/controls/PublicIDControl.java
@@ -1,8 +1,10 @@
 package dev.ikm.komet.kview.controls;
 
 import dev.ikm.komet.kview.controls.skin.PublicIDControlSkin;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
 
@@ -11,26 +13,33 @@ import javafx.scene.control.Skin;
 public class PublicIDControl extends Control {
 
      /// The public ID (UUID) property
-    private SimpleStringProperty publicId = new SimpleStringProperty(this, "publicId");
+    private StringProperty publicId = new SimpleStringProperty(this, "publicId");
     /// The showLabel property which controls if the ID label is displayed
-    private SimpleBooleanProperty showLabel = new SimpleBooleanProperty(this, "showLabel", true);
+    private BooleanProperty showLabel = new SimpleBooleanProperty(this, "showLabel", true);
+    /// The trimIdentifier tells the PublicIDControlSkin to trim the identifier value from the colon in the prefix
+    private BooleanProperty trimIdentifier = new SimpleBooleanProperty(this, "trimIdentifier", false);
 
     public PublicIDControl() {
          super();
     }
 
-    public PublicIDControl(boolean showLabel, String publicId) {
+    public PublicIDControl(boolean showLabel, String publicId, boolean trimIdentifier) {
         super();
         setPublicId(publicId);
         setShowLabel(showLabel);
+        setTrimIdentifier(trimIdentifier);
     }
 
-    public SimpleStringProperty publicIdProperty() {
+    public StringProperty publicIdProperty() {
         return publicId;
     }
 
-    public SimpleBooleanProperty showLabelProperty() {
+    public BooleanProperty showLabelProperty() {
         return showLabel;
+    }
+
+    public BooleanProperty trimIdentifierProperty() {
+        return trimIdentifier;
     }
 
     public String getPublicId() {
@@ -47,6 +56,14 @@ public class PublicIDControl extends Control {
 
     public void setShowLabel(boolean showLabel) {
         showLabelProperty().set(showLabel);
+    }
+
+    public boolean getTrimIdentifier() {
+        return trimIdentifierProperty().get();
+    }
+
+    public void setTrimIdentifier(boolean trimIdentifier) {
+        trimIdentifierProperty().set(trimIdentifier);
     }
 
     /** {@inheritDoc} */
