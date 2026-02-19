3fe572e2241a193e9909edf5c16c043591305be7
Carl Dea <carl.dea@gmail.com>
2026-02-18 23:17:27 -0500

    New support to change the main edit coordinates (#860)
    * Update version to start feature: FVKB-190-edit-coordinate-preferences
    
    * Initial Configure Edit Coordinates Popup
    
    * Initial display of edit coordinate options. Author for change, default mod, dest mod, default path, promotion path
    
    * New support to change the main Edit Coordinates for transactions. Added context menu to add available options to a section edit coordinate field.
    
    * Update version to finish feature: FVKB-190-edit-coordinate-preferences
    # Conflicts:
    #	kview/pom.xml
    #	kview/src/main/java/dev/ikm/komet/kview/mvvm/view/landingpage/LandingPageController.java

---
+++ kview/src/main/java/dev/ikm/komet/kview/controls/LangEditCoordinateTitledPane.java
@@ -0,0 +1,54 @@
+package dev.ikm.komet.kview.controls;
+
+import dev.ikm.komet.kview.controls.skin.LangEditCoordinateTitledPaneSkin;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.control.Skin;
+
+public class LangEditCoordinateTitledPane extends FilterTitledPane {
+
+    public LangEditCoordinateTitledPane() {
+        getStyleClass().add("lang-filter-titled-pane");
+    }
+
+    // ordinalProperty
+    private final IntegerProperty ordinalProperty = new SimpleIntegerProperty(this, "ordinal");
+    public final IntegerProperty ordinalProperty() {
+       return ordinalProperty;
+    }
+    public final int getOrdinal() {
+       return ordinalProperty.get();
+    }
+    public final void setOrdinal(int value) {
+        ordinalProperty.set(value);
+    }
+
+//    // defaultLangCoordinatesProperty
+//    private final ObjectProperty<EditCoordinateOptions.LanguageFilterCoordinates> defaultLangCoordinatesProperty = new SimpleObjectProperty<>(this, "defaultLangCoordinates");
+//    public final ObjectProperty<EditCoordinateOptions.LanguageFilterCoordinates> defaultLangCoordinatesProperty() {
+//       return defaultLangCoordinatesProperty;
+//    }
+//    public final EditCoordinateOptions.LanguageFilterCoordinates getDefaultLangCoordinates() {
+//       return defaultLangCoordinatesProperty.get();
+//    }
+//    public final void setDefaultLangCoordinates(EditCoordinateOptions.LanguageFilterCoordinates value) {
+//        defaultLangCoordinatesProperty.set(value);
+//    }
+//
+//    // langCoordinatesProperty
+//    private final ObjectProperty<EditCoordinateOptions.LanguageFilterCoordinates> langCoordinatesProperty = new SimpleObjectProperty<>(this, "langCoordinates");
+//    public final ObjectProperty<EditCoordinateOptions.LanguageFilterCoordinates> langCoordinatesProperty() {
+//       return langCoordinatesProperty;
+//    }
+//    public final EditCoordinateOptions.LanguageFilterCoordinates getLangCoordinates() {
+//       return langCoordinatesProperty.get();
+//    }
+//    public final void setLangCoordinates(EditCoordinateOptions.LanguageFilterCoordinates value) {
+//        langCoordinatesProperty.set(value);
+//    }
+
+    @Override
+    protected Skin<?> createDefaultSkin() {
+        return new LangEditCoordinateTitledPaneSkin(this);
+    }
+}
